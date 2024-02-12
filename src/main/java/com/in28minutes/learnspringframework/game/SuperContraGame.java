a491eab3c6f1b46248b334368e9751471eeb5bce
vikash1012 <vikashbihari10122001@gmail.com>
2024-02-12 16:11:18 +0530

    add gameRunner with interface for loose coupling

--- src/main/java/com/in28minutes/learnspringframework/game/SuperContraGame.java
+++ src/main/java/com/in28minutes/learnspringframework/game/SuperContraGame.java
@@ -1,19 +1,17 @@
 package com.in28minutes.learnspringframework.game;
 
-public class MarioGame implements GamingConsole {
+public class SuperContraGame implements GamingConsole{
 	public void up() {
-		System.out.println("Jump");
+		System.out.println("Up");
 	}
 	public void down() {
-		System.out.println("Go into the hole");
+		System.out.println("Sit Down");
 	}
 	public void left() {
 		System.out.println("Go back");
 	}
 	public void right() {
-		System.out.println("Accelerate");
+		System.out.println("shoot the bullet");
 	}
-
-
 
 }
