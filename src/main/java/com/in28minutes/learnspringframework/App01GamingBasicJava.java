d08ea6ee07f593dc78e5e1f33ec20b3e5a8f15c8
vikash1012 <vikashbihari10122001@gmail.com>
2024-02-12 16:44:11 +0530

    add spring context and configuration file

--- src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
+++ src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
@@ -4,12 +4,14 @@ import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.MarioGame;
 import com.in28minutes.learnspringframework.game.SuperContraGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
 
 	public static void main(String[] args) {
-		var game = new MarioGame();
-//		var game = new SuperContraGame();
-		var gameRunner = new GameRunner(game);
+//		var game = new MarioGame();
+		var game = new SuperContraGame(); // 1: Object Creation
+		var gameRunner = new GameRunner(game); // 2: Object Creation + Wiring of Dependencies
+		//Game is a dependency of GameRunner
+		
 		gameRunner.run();
 
 	}
---
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -0,0 +1,19 @@
+package com.in28minutes.learnspringframework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App02HelloWorldSpring {
+
+	public static void main(String[] args) {
+		//1: Launch a Spring Context- application
+		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+		
+		//2: Configure the things that we want Spring to manage -
+		//  HelloWorldConfigrationClass- @Configuration
+		//name method- @Bean
+		System.out.println(context.getBean("name"));
+		
+
+	}
+
+}
