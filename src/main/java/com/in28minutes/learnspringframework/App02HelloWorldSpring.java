f822beffa90595577399a3f7753cff80494e9319
vikash1012 <vikashbihari10122001@gmail.com>
2024-02-21 11:57:30 +0530

    add custom class bean

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -12,7 +12,7 @@ public class App02HelloWorldSpring {
 		//  HelloWorldConfigrationClass- @Configuration
 		//name method- @Bean
 		System.out.println(context.getBean("name"));
-		
+		System.out.println(context.getBean("address"));
 
 	}
 
--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,6 +3,9 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Address(String firstLine, String City) {
+	
+}
 @Configuration
 public class HelloWorldConfiguration {
 	@Bean
@@ -10,4 +13,8 @@ public class HelloWorldConfiguration {
 		return "Vikash";
 	}
 
+	@Bean
+	public Address address() {
+		return new Address("Kolhachak", "Masaurhi");
+	}
 }
